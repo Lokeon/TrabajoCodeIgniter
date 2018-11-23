8829fce3853146263fd470e4e36e764ca44c6ef2
Lokeon <kevin.lopezcala99@gmail.com>
2018-11-23 20:42:01 +0100

    :penguin:

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost; dbname=review; charset=utf8;',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'review',
-	'dbdriver' => 'mysqli',
+	'database' => '',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
--- application/controllers/Login.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Login extends CI_Controller
-{
-
-}
