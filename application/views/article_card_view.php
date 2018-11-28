1fb7f2098582d3e2759fcb352e7fbaf99908b9d7
Borja <1415.romero.borja@gmail.com>
2018-11-28 12:34:00 +0100

    estrellitas

--- application/views/article_card_view.php
+++ application/views/article_card_view.php
@@ -1,21 +1,20 @@
 <div class="card" style="width: 18rem; padding: 0 12px 14px">
-    <img class="card-img-top" src="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201710/23/00194611000301____1__640x640.jpg" alt="Imagen">
+    <img class="card-img-top" src="{image}" alt="Imagen">
     <div class="card-body">
-        <h5 class="card-title"> <?=$nombreproducto = "Producto"?> </h5>
+        <h5 class="card-title">{nombreproducto}</h5>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="star-rating">
-                        <span class="fa fa-star-o" data-rating="1"></span>
-                        <span class="fa fa-star-o" data-rating="2"></span>
-                        <span class="fa fa-star-o" data-rating="3"></span>
-                        <span class="fa fa-star-o" data-rating="4"></span>
-                        <span class="fa fa-star-o" data-rating="5"></span>
-                        <input type="hidden" name="whatever1" class="rating-value" value="2.56">
-                    </div>
+                <div class="star-rating ml-0">
+                    <span class="fa fa-star-o" data-rating="1"></span>
+                    <span class="fa fa-star-o" data-rating="2"></span>
+                    <span class="fa fa-star-o" data-rating="3"></span>
+                    <span class="fa fa-star-o" data-rating="4"></span>
+                    <span class="fa fa-star-o" data-rating="5"></span>
+                    <input type="hidden" name="whatever1" class="rating-value" value="{media}">
                 </div>
             </div>
         </div>
-        <a href="#" class="btn btn-primary">Ver artículo</a>
+        <br>
+        <a href="{linkarticulo}" class="btn btn-primary">Ver artículo</a>
     </div>
 </div>
