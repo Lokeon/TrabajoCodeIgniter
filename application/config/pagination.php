da0e36582145cf5067698e6aadfa4edb1dd67d2e
KROSF <rodrigosanabria22@gmail.com>
2018-11-29 18:09:33 +0100

    change email priority

---
+++ application/config/pagination.php
@@ -0,0 +1,11 @@
+<?php
+$config = array(
+    'base_url' => '',
+    'total_rows' => 3,
+    'per_page' => 10,
+    'use_page_numbers' => true,
+    'num_links' => 'empty',
+    'cur_tag_open' => '&nbsp;<a class="current">',
+    'cur_tag_close' => '</a>',
+    'next_link' => 'Siguiente',
+);
--- application/models/Articles_model.php
+++ application/models/Articles_model.php
@@ -1,8 +1,13 @@
 <?php
-class Articles extends CI_Controller
+class Articles_model extends CI_Model
 {
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function getArticles()
+    {
+
     }
 }
