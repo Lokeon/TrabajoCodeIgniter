e258eeeb8d43a5e3d8dcfa9e82d5b1d798a6ec95
KROSF <rodrigosanabria22@gmail.com>
2018-12-03 13:28:31 +0100

    articles sample

--- application/models/Articles_model.php
+++ application/models/Articles_model.php
@@ -29,7 +29,7 @@ class Articles_model extends CI_Model
     public function getStar($id_article, $id_user = null)
     {
         $this->db->select('ROUND(AVG(stars),1) as meanStars');
-        $this->db->from('commets');
+        $this->db->from('comments');
         $this->db->where('id_article', $id_article);
         $mean = $this->db->get()->result_array()[0]['meanStars'];
         return ($mean == '') ? 0 : $mean;
