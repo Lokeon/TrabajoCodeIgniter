550854fb56d4211b219ef1465c0632c0ca21498d
KROSF <rodrigosanabria22@gmail.com>
2018-12-03 15:46:24 +0100

    articles sample

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -14,7 +14,12 @@ class Articles extends CI_Controller
     public function testArticleUser()
     {
         print($this->articles_model->getStarUser(1, 1));
-        $this->articles_model->updateStar(1, 4);
+        $this->articles_model->updateStar(1, 1);
         print($this->articles_model->getStarUser(1, 1));
+    }
+
+    public function article($id)
+    {
+        echo $id;
     }
 }
