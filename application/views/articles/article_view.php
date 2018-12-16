3c88016f1f5b3443f579e26b8f453e31d9be7795
Borja <1415.romero.borja@gmail.com>
2018-12-16 20:01:03 +0100

    update

--- application/views/articles/article_view.php
+++ application/views/articles/article_view.php
@@ -1,18 +1,18 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="container" style="padding-top:20px">
     <div class="row">
-        <div class="card flex-row flex-wrap">
+        <div class="card flex-row flex-wrap text-center">
             <div class="card-header border-0">
                 <img src="<?=$image ?>" width="300px" height="300px">
             </div>
             <div class="card-block px-2 pt-3">
-                <h4 class="card-title ">
-                    <h2><?=$name ?></h2>
-                </h4>
+                <h2 class="card-title">
+                    <?=$name ?>
+                </h2>
+                <div class="card-block px-2">
+                    <div id="star-0" class="stars"></div>
+                <div>
             </div>
-            <div class="card-block px-2">
-                <div id="star-0" class="stars"></div>
-            <div>
             <div class="card-block px-2 pb-5">
                 <p class="card-text">
                     <?=$description ?>
