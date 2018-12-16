3bf06bc8161823e008f386f130401892233da773
Borja <1415.romero.borja@gmail.com>
2018-12-16 17:51:03 +0100

    update

--- application/views/admin/comments_view.php
+++ application/views/admin/comments_view.php
@@ -1,2 +1,21 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<div
+<div class="container">
+    <div class="row">
+        <div class="col-sm-14">
+            <div class="panel panel-white post panel-shadow">
+                <div class="post-heading">
+                    <div class="meta">
+                        <div class="title h5">
+                            <b>{usuario}</b>
+                        </div>
+                        <h6 class="text-muted time">{fecha}</h6>
+                        {estrellas}
+                    </div>
+                </div>
+                <div class="post-description">
+                    <p>{comentario}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
