ecd520172cac7a32baca4abad40b2008fc6cc0c8
Borja <1415.romero.borja@gmail.com>
2018-12-17 15:24:32 +0100

    update

--- application/views/articles/article_view.php
+++ application/views/articles/article_view.php
@@ -1,33 +1,34 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="container" style="padding-top:20px">
     <div class="row">
-        <div class="card flex-row flex-wrap">
-            <div class="card-header border-0">
-                <img src="<?=$image ?>" width="300px" height="300px">
+        <div class="card bg-white shadow" style="width:100%">
+            <div class="container-fluid py-3">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="//c1.staticflickr.com/5/4811/31389348647_22158c1637.jpg" class="img-fluid" width="400px" height="400px">
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Artículo</h3>
+                        <div id="star-0" class="stars"></div>
+                        <br>
+                        <ul style="padding-left:20px; font-size:1.2rem">
+                            <?php
+                                $s = explode('*', "Es muy bonito*Es pequeño*Es rojo*Me gusta*Comentario 1*Comentario 2*Comentario 3*Comentario 4*Comentario 5");
+                                foreach ($s as $string) {
+                                    print ("<li>$string</li>");
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="card-block px-2 pt-3">
-                <h4 class="card-title ">
-                    <h2><?=$name ?></h2>
-                </h4>
-            </div>
-            <div id="star-0" class="stars"></div>
-            <div class="card-block px-2 pb-5">
-                <p class="list-group">
-                <?php
-                    $s = explode('*', $description);
-                    foreach ($s as $key) {
-                        print("-" . $key . "<br/>");
-                    }
-                ; ?>
-                </p>
-            </div>
-            <ul class="list-group">
-                <?php
-                    for ($i = 0; $i < count($comments); ++$i) {
-                        print("<li class='list-group-item'>{$comments[$i]['comment']}</li>");
-                    }
-                ?>
-            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="">
+            <?php
+                
+            ?>
         </div>
     </div>
 </div>
--- application/views/articles/articles_card.php
+++ application/views/articles/articles_card.php
@@ -3,7 +3,6 @@
     <img class="card-img-top" src="{image}" alt="Imagen">
     <div class="card-body">
         <h5 class="card-title">{name}</h5>
-        <p class="card-text"><i class="far fa-star"></i>Some example text.</p>
         <div id="star-{index}" class="stars">
         </div>
         <a href="{url}" class="btn btn-primary">Ver artículo</a>
