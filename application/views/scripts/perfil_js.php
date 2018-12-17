392ce45ed0458363d55c7d1f555ae12526ec70db
Lokeon <kevin.lopezcala99@gmail.com>
2018-12-17 15:58:30 +0100

    :penguin:

--- application/views/scripts/perfil_js.php
+++ application/views/scripts/perfil_js.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <script>
 $(document).ready(function() {
-    $('#username').on('click', function () {
-        $('#user').add(`
+    $('#username').one('click', function () {
+        $('#user').append(`
         <div class="form-group">
             <input type="password" class="form-control" placeholder="Contraseña" onclick="">
         </div>
