ccccbfec9dad634ad61f1e7bfca8d48f9a402522
KROSF <rodrigosanabria22@gmail.com>
2018-12-17 12:41:48 +0100

    fixed stars

--- application/views/articles/article_view.php
+++ application/views/articles/article_view.php
@@ -3,27 +3,29 @@
     <div class="row">
         <div class="card flex-row flex-wrap text-center">
             <div class="card-header border-0">
-                <img src="<?= $image ?>" width="300px" height="300px">
+                <img src="<?=$image ?>" width="300px" height="300px">
             </div>
             <div class="card-block px-2 pt-3">
                 <h4 class="card-title ">
-                    <h2><?= $name ?></h2>
+                    <h2><?=$name ?></h2>
                 </h4>
             </div>
+            <div id="star-0" class="stars"></div>
             <div class="card-block px-2 pb-5">
                 <p class="list-group">
                 <?php
-                $s = explode('*', $description);
-                foreach ($s as $key) {
-                    print("-" . $key . "<br/>");
-                }; ?>
+                    $s = explode('*', $description);
+                    foreach ($s as $key) {
+                        print("-" . $key . "<br/>");
+                    }
+                ; ?>
                 </p>
             </div>
             <ul class="list-group">
                 <?php
-                for ($i = 0; $i < count($comments); ++$i) {
-                    print("<li class='list-group-item'>{$comments[$i]['comment']}</li>");
-                }
+                    for ($i = 0; $i < count($comments); ++$i) {
+                        print("<li class='list-group-item'>{$comments[$i]['comment']}</li>");
+                    }
                 ?>
             </ul>
         </div>
